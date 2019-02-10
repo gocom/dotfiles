38cf2683d66b0607a67ad1defa3aa69368aaad94
Jukka Svahn <void@rah.pw>
2019-02-10 05:30:34 +0200

    Dotfile bins take presence

--- lib/module/0/path.bash
+++ lib/module/0/path.bash
@@ -1,9 +1,9 @@
 df::path --prepend "$HOME/bin" "$HOME/.local/bin" \
     "/usr/local/bin" "/usr/local/sbin" "/usr/local/games" \
+    "$DOTFILES_HOME/platform/$DOTFILES_PLATFORM/bin" \
+    "$DOTFILES_HOME/bin" \
     "/usr/bin" "/usr/sbin" "/usr/games" \
     "/bin" "/sbin" "/snap/bin"
 
-df::path "$DOTFILES_HOME/bin" \
-  "$DOTFILES_HOME/platform/$DOTFILES_PLATFORM/bin" \
-  "/Applications/Firefox Developer Edition.app/Contents/MacOS" \
+df::path "/Applications/Firefox Developer Edition.app/Contents/MacOS" \
   "/Applications/Firefox.app/Contents/MacOS"
