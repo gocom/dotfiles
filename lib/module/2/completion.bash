0f9d96dc7c41acb52f42ffdcd35917927219bf2a
Jukka Svahn <void@rah.pw>
2019-02-19 21:03:25 +0200

    Fix autocompletion support

--- lib/module/2/completion.bash
+++ lib/module/2/completion.bash
@@ -3,6 +3,10 @@ shopt -s complete_fullquote
 shopt -s dirspell
 shopt -s hostcomplete
 
+if [ -d "/usr/local/etc/bash_completion.d" ]; then
+  BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+fi
+
 if [ -f "/usr/local/share/bash-completion/bash_completion" ]; then
   . "/usr/local/share/bash-completion/bash_completion"
 elif [ -f "/usr/local/etc/bash_completion" ]; then
