44672f1456bb3bf05b53b41904b77cf89acce8d7
Jukka Svahn <jukka@@lamia.fi>
2021-01-24 03:42:00 +0200

    Support nvm

--- lib/module/2/node.bash
+++ lib/module/2/node.bash
@@ -1,1 +1,12 @@
 df::path "$DOTFILES_HOME/node_modules/.bin"
+
+export NVM_DIR="$HOME/.nvm"
+
+if [ -e "$NVM_DIR/nvm.sh" ]; then
+    . "$NVM_DIR/nvm.sh"
+fi
+
+if [ -e "$NVM_DIR/bash_completion" ]; then
+    . "$NVM_DIR/nvm.sh"
+fi
+
