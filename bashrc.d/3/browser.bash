f428d34567944a2ed00f302cb2228180d1c5245c
Jukka Svahn <void@rah.pw>
2026-06-24 06:36:22 +0300

    Some WSL compatibility updates
    Copies mandoc stylesheet, as the symlink can not work on Windows side

--- bashrc.d/3/browser.bash
+++ bashrc.d/3/browser.bash
@@ -1,14 +1,16 @@
 if [ "$DOTFILES_DISPLAY" ]; then
-  if [ -x "$(command -v open)" ]; then
-    export BROWSER="open"
-  elif [ -x "$(command -v "chrome-cli")" ]; then
+  if [ -x "$(command -v "chrome-cli")" ]; then
     export BROWSER="chrome-cli open"
   elif [ -x "$(command -v google-chrome)" ]; then
     export BROWSER="google-chrome"
   elif [ -x "$(command -v chromium-browser)" ]; then
     export BROWSER="chromium-browser"
+  elif [ -x "$(command -v chrome)" ]; then
+    export BROWSER="chrome"
   elif [ -x "$(command -v firefox)" ]; then
     export BROWSER="firefox"
+  elif [ -x "$(command -v open)" ]; then
+    export BROWSER="open"
   fi
 fi
 
